b026a2c2e72c8dbddd97d4714062e37f798f142c
Bernie <b4456609@gmail.com>
2017-01-14 09:21:54 +0800

    add entity

---
+++ src/main/java/soselab/mpg/model/graph/OwnRelationship.java
@@ -0,0 +1,11 @@
+package soselab.mpg.model.graph;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+
+@RelationshipEntity(type = "OWN")
+public class OwnRelationship {
+    @GraphId
+    private Long id;
+
+}
