22b8372f16431532f6bdae4762bfa62e316ef0d5
Bernie <b4456609@gmail.com>
2017-02-19 18:12:16 +0800

    remove no need field

--- src/main/java/soselab/mpg/graph/service/MicroserviceGraphBuilderServiceImpl.java
+++ src/main/java/soselab/mpg/graph/service/MicroserviceGraphBuilderServiceImpl.java
@@ -4,9 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import soselab.mpg.graph.repository.EndpointNodeRepository;
-import soselab.mpg.graph.repository.ScenarioNodeRepository;
-import soselab.mpg.graph.repository.ServiceNodeRepository;
 import soselab.mpg.graph.service.handler.GraphBuildHandler;
 import soselab.mpg.graph.service.handler.ScenarioBuildHandler;
 import soselab.mpg.graph.service.handler.ServiceBuildHandler;
@@ -19,22 +16,13 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class MicroserviceGraphBuilderServiceImpl implements MicroserviceGraphBuilderService {
     private static final Logger LOGGER = LoggerFactory.getLogger(MicroserviceGraphBuilderServiceImpl.class);
     private final AtomicBoolean needBuild = new AtomicBoolean(false);
-    private final EndpointNodeRepository endpointNodeRepository;
-    private final ScenarioNodeRepository scenarioNodeRepository;
-    private final ServiceNodeRepository serviceNodeRepository;
     private final ServiceBuildHandler serviceBuildHandler;
     private final ScenarioBuildHandler scenarioBuildHandler;
     private Thread thread = null;
 
     @Autowired
-    public MicroserviceGraphBuilderServiceImpl(EndpointNodeRepository endpointNodeRepository,
-                                               ScenarioNodeRepository scenarioNodeRepository,
-                                               ServiceNodeRepository serviceNodeRepository,
-                                               ServiceBuildHandler serviceBuildHandler,
+    public MicroserviceGraphBuilderServiceImpl(ServiceBuildHandler serviceBuildHandler,
                                                ScenarioBuildHandler scenarioBuildHandler) {
-        this.endpointNodeRepository = endpointNodeRepository;
-        this.scenarioNodeRepository = scenarioNodeRepository;
-        this.serviceNodeRepository = serviceNodeRepository;
         this.serviceBuildHandler = serviceBuildHandler;
         this.scenarioBuildHandler = scenarioBuildHandler;
     }
@@ -48,13 +36,12 @@ public class MicroserviceGraphBuilderServiceImpl implements MicroserviceGraphBui
         }
     }
 
-    private synchronized void start() {
+    private void start() {
         this.thread = new Thread(this::buildGraph);
         thread.start();
     }
 
     private void buildGraph() {
-
         // the execute order, the scenario is depend on service build result
         List<GraphBuildHandler> graphBuildHandlers = new ArrayList<>();
         graphBuildHandlers.add(serviceBuildHandler);
