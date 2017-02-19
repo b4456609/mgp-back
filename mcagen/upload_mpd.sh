ef60e88138943cfd1b1cf5fe955e73a488373022
Bernie <b4456609@gmail.com>
2017-02-19 18:18:31 +0800

    bash upload mpd parallel

--- mcagen/upload_mpd.sh
+++ mcagen/upload_mpd.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+count=0
+total=0
 for file in `find ./build/output -type f`
 do
-	curl -i -X POST \
+	curl -X POST \
 	-H "Content-Type: multipart/form-data" \
-	-F "file=@./$file" http://140.121.102.164/api/upload
+	-F "file=@./$file" http://140.121.102.164/api/upload &
 #	-F "file=@./$file" http://localhost:8080/api/upload
+    ((count++))
+    ((total++))
+    echo "$total"
+    if [ "$count" == "10" ]; then
+        count=0
+        wait
+    fi
 
 done
