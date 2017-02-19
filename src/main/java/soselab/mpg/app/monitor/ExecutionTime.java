d094ef611f74ded8e69fc2cfc892742517f7d657
Bernie <b4456609@gmail.com>
2017-02-19 20:23:08 +0800

    monitor time

---
+++ src/main/java/soselab/mpg/app/monitor/ExecutionTime.java
@@ -0,0 +1,25 @@
+package soselab.mpg.app.monitor;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ExecutionTime {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExecutionTime.class);
+
+    @Around("execution(* soselab.mpg.graph.service.*Service*.*(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+        String className = pjp.getSignature().getDeclaringTypeName();
+        String methodName = pjp.getSignature().getName();
+        long start = System.currentTimeMillis();
+        Object proceed = pjp.proceed();
+        long elapsedTime = System.currentTimeMillis() - start;
+        LOGGER.info("Method {}.{}() execution time : {} ms", className, methodName, elapsedTime);
+        return proceed;
+    }
+}
