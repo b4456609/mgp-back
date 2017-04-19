af20834a38088f3bbdae9faaafbc386469ddab05
Bernie <b4456609@gmail.com>
2017-04-19 21:26:23 +0800

    remove unnecessary variable

--- src/main/java/soselab/mpg/regression/controller/RegressionController.java
+++ src/main/java/soselab/mpg/regression/controller/RegressionController.java
@@ -4,9 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import soselab.mpg.bdd.service.BDDService;
-import soselab.mpg.graph.service.GraphService;
-import soselab.mpg.pact.service.PactService;
 import soselab.mpg.regression.controller.dto.PactTestCaseDTO;
 import soselab.mpg.regression.service.ServiceTestStrategy;
 import soselab.mpg.regression.service.UATStrategy;
@@ -17,19 +14,13 @@ import java.util.List;
 @RequestMapping("/api/regression")
 public class RegressionController {
     private static final Logger LOGGER = LoggerFactory.getLogger(RegressionController.class);
-    private final GraphService graphService;
-    private final PactService pactService;
-    private final BDDService bddService;
     private final ServiceTestStrategy serviceTestStrategy;
     private final UATStrategy uatStrategy;
 
 
     @Autowired
-    public RegressionController(GraphService graphService, PactService pactService, BDDService bddService, ServiceTestStrategy serviceTestStrategy, UATStrategy uatStrategy) {
+    public RegressionController(ServiceTestStrategy serviceTestStrategy, UATStrategy uatStrategy) {
         this.serviceTestStrategy = serviceTestStrategy;
-        this.graphService = graphService;
-        this.pactService = pactService;
-        this.bddService = bddService;
         this.uatStrategy = uatStrategy;
     }
 
